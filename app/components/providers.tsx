f19ba7957c7dd792dc5b310f301c7a0dec31fc9e
Utkarsh Jaiswal <120572477+Zyaxxy@users.noreply.github.com>
2026-03-03 23:13:05 +0530

    Clean up commented code in providers.tsx
    Removed commented-out code for the DynamicContextProvider component.

--- app/components/providers.tsx
+++ app/components/providers.tsx
@@ -10,20 +10,6 @@
    
 //   }
   
-// import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
-
-// export function Providers({ children }: { children: React.ReactNode }) {
-//   return (
-//     <DynamicContextProvider
-//       settings={{
-//         environmentId: process.env.NEXT_PUBLIC_DYNAMIC_ENV_ID!,
-//       }}
-//     >
-//       {children}
-//     </DynamicContextProvider>
-//   );
-// }
-
 import {
   DynamicContextProvider,
   DynamicWidget,
@@ -36,20 +22,6 @@ import { SolanaWalletConnectors } from "@dynamic-labs/solana";
 type ProvidersProps = {
   children: ReactNode;
 };
-
-
-// export default function Providers({ children }: ProvidersProps)  {
-//   return (
-//     <DynamicContextProvider
-//       settings={{
-//         environmentId: "VAL",
-//         walletConnectors: [SolanaWalletConnectors],
-//       }}
-//     >
-//       <DynamicWidget />
-//     </DynamicContextProvider>
-//   );
-// }
 
 
 export function Providers({ children }: ProvidersProps) {
