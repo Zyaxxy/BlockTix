25b3968f52e63cb9d65e470c726f709ba403d018
Utkarsh <utkarshjaiswal285@gmail.com>
2026-03-03 21:45:22 +0530

    Updated Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -2,34 +2,23 @@
 
 import Link from "next/link";
 import Ballpit from "./components/Ballpit";
+import HeroContent from "./components/ui/HeroContent";
 
 export default function Home() {
   return (
     <div className="relative h-screen w-screen overflow-hidden bg-black text-white">
-      
+
       <Ballpit
-        count={130}
-        gravity={0.5}
-        friction={0.965}
-        wallBounce={0.5}
+        count={100}
+        gravity={0.05}
+        friction={0.9975}
+        wallBounce={0.95}
         followCursor={false}
         colors={["#3c0fa3", "#00eb85"]}
       />
 
       <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
-        <h1 className="text-4xl md:text-6xl font-bold mb-6">
-          Block-Tix
-        </h1>
-
-        <p className="text-zinc-400 mb-8 max-w-md">
-          Secure NFT-based ticketing on Solana.
-        </p>
-
-        <Link href="/login">
-          <button className="px-8 py-3 rounded-xl bg-gradient-to-r from-indigo-600 to-emerald-500 hover:opacity-90 transition text-lg">
-            Get Started
-          </button>
-        </Link>
+        <HeroContent />
       </div>
     </div>
   );
