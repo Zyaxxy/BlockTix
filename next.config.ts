78b155365ace85b2ff1c214525b5dc93c92bd376
Utkarsh <utkarshjaiswal285@gmail.com>
2026-03-07 15:05:12 +0530

    Build Issues Fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["ws"],
+  serverExternalPackages: ["ws", 'pino', 'pino-pretty', 'thread-stream'],
 };
 
 export default nextConfig;
